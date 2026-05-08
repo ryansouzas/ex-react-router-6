6843ac89f8c5f70bfa2b0ed6d35052e70aab5b0a
Ryan Souza <ryanssebadelhe@gmail.com>
2026-05-08 11:53:18 -0300

    ex1 - HomeCard Component

--- exercicio1/src/App.tsx
+++ exercicio1/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import HomeCard from "./components/HomeCard";
 
 function App() {
   return (
     <>
       <Header />
+      <HomeCard />
     </>
   );
 }
---
+++ exercicio1/src/components/HomeCard/index.tsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function HomeCard() {
+  return (
+    <div className="app-container app-mt20 app-home-container">
+      <h2>Página inicial</h2>
+    </div>
+  );
+}
