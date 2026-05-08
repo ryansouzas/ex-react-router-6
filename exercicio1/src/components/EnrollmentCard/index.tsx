bde4e5a24236444f69e3da662b3c5ebb424c5cde
Ryan Souza <ryanssebadelhe@gmail.com>
2026-05-08 15:05:58 -0300

    ex1 - Enrrolment route

---
+++ exercicio1/src/components/EnrollmentCard/index.tsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function EnrollmentCard() {
+  return (
+    <div className="app-home-container">
+      <h2>Página de Inscrição</h2>
+    </div>
+  );
+}
---
+++ exercicio1/src/routes/Enrollment/index.tsx
@@ -0,0 +1,15 @@
+import ButtonEnrollment from "../../components/ButtonEnrollment";
+import EnrollmentCard from "../../components/EnrollmentCard";
+
+export default function Enrollment() {
+  return (
+    <>
+      <main>
+        <section className="app-container app-mt20">
+          <EnrollmentCard />
+          <ButtonEnrollment />
+        </section>
+      </main>
+    </>
+  );
+}
