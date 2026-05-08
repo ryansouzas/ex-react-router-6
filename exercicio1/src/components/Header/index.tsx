e81dc50863fcaaefb448bfa02722d79e9c336ef9
Ryan Souza <ryanssebadelhe@gmail.com>
2026-05-08 11:29:18 -0300

    ex1 - Header Component

---
+++ exercicio1/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <header className="app-header">
+      <nav className="app-container">
+        <h1>MeuSite</h1>
+      </nav>
+    </header>
+  );
+}
